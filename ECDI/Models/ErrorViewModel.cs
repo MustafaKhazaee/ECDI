c763df7a742bf24f229a57116d227ac3c3e158da
MustafaKhazaee <101381823+MustafaKhazaee@users.noreply.github.com>
2022-05-11 14:35:56 +0430

    Add project files.

---
+++ ECDI/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace ECDI.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
