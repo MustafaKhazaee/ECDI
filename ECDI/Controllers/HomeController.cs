0ca334f1acbcd1c7386cbabddc83701ddefc20b9
MustafaKhazaee <101381823+MustafaKhazaee@users.noreply.github.com>
2022-05-15 12:47:47 +0430

    Added New Pages

--- ECDI/Controllers/HomeController.cs
+++ ECDI/Controllers/HomeController.cs
@@ -1,23 +1,27 @@
-﻿using ECDI.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-
+﻿using Microsoft.AspNetCore.Mvc;
 namespace ECDI.Controllers {
     public class HomeController : Controller {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger) {
-            _logger = logger;
-        }
-
         public IActionResult Index() {
             return View();
         }
-
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Courses() {
+            return View();
+        }
+        public IActionResult CourseDetails () {
+            return View();
+        }
+        public IActionResult TeacherProfile () {
+            return View();
+        }
+        public IActionResult Login () {
+            ViewBag.dontShow = true;
+            return View();
+        }
+        public IActionResult AboutUs () {
+            return View();
+        }
+        public IActionResult ContactUs () {
+            return View();
         }
     }
 }
