ea34d4f7b36cd3e11d2b51f24e47c93e659fb6aa
MustafaKhazaee <101381823+MustafaKhazaee@users.noreply.github.com>
2022-05-15 14:22:52 +0430

    adf

---
+++ ECDI/Migrations/20220515095117_b.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECDI.Migrations
+{
+    public partial class b : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Mobile1",
+                table: "Teachers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Teachers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Teachers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Introduction",
+                table: "Teachers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Introduction",
+                table: "Teachers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mobile1",
+                table: "Teachers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LastName",
+                table: "Teachers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Teachers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- ECDI/Models/Teacher.cs
+++ ECDI/Models/Teacher.cs
@@ -2,9 +2,10 @@
     public class Teacher {
         public int Id { get; set; }
         public string FirstName { get; set; }
-        public int LastName { get; set; }
-        public string Email { get ; set; }
-        public string Mobile1 { get; set; }
+        public string LastName { get; set; }
+        public string? Introduction { get; set; }
+        public string? Email { get ; set; }
+        public string? Mobile1 { get; set; }
         public string? Mobile2 { get; set; }
     }
 }
